56da35ee0bb2dccea73a6858c48a3c2eaef4c12f
psmoreiraa <100532245+psmoreiraa@users.noreply.github.com>
2026-05-13 15:19:05 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,53 +1,27 @@
-// TERMINAL
-const input = document.getElementById("input");
-const output = document.getElementById("output");
+document.addEventListener("DOMContentLoaded", () => {
 
-input.addEventListener("keypress", function(e) {
-  if (e.key === "Enter") {
-    const command = input.value.toLowerCase();
-    let response = "";
+  const input = document.getElementById("input");
+  const output = document.getElementById("output");
 
-    if (command === "whoami") {
-      response = "Pedro Moreira - estudante de Engenharia da Computação";
-    } else if (command === "skills") {
-      response = "HTML, CSS, C/C++, Excel, Canva";
-    } else if (command === "contact") {
-      response = "Email: pedrosmoreira34@gmail.com";
-    } else {
-      response = "Comando não reconhecido";
-    }
+  input.addEventListener("keydown", function(e) {
+    if (e.key === "Enter") {
 
-    output.innerHTML += `<p>> ${command}</p><p>${response}</p>`;
-    input.value = "";
-  }
-});
+      const command = input.value.toLowerCase().trim();
+      let response = "";
 
+      if (command === "whoami") {
+        response = "Pedro Moreira - Engenharia da Computação";
+      } else if (command === "skills") {
+        response = "HTML, CSS, C/C++, Excel, Canva";
+      } else if (command === "contact") {
+        response = "Email: pedrosmoreira34@gmail.com";
+      } else {
+        response = "Comando não reconhecido";
+      }
 
-// COPIAR EMAIL
-const email = "pedrosmoreira34@gmail.com";
-
-document.querySelector("#contato p").addEventListener("click", () => {
-  navigator.clipboard.writeText(email);
-  alert("Email copiado!");
-});
-
-
-// ANIMAÇÃO AO ROLAR
-const sections = document.querySelectorAll("section");
-
-const observer = new IntersectionObserver((entries) => {
-  entries.forEach(entry => {
-    if (entry.isIntersecting) {
-      entry.target.style.opacity = 1;
-      entry.target.style.transform = "translateY(0)";
+      output.innerHTML += `<p>> ${command}</p><p>${response}</p>`;
+      input.value = "";
     }
   });
-});
 
-sections.forEach(sec => {
-  sec.style.opacity = 0;
-  sec.style.transform = "translateY(20px)";
-  sec.style.transition = "0.5s";
-
-  observer.observe(sec);
 });
